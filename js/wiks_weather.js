c6efbe85a3a2501c487214f454aa0bd2a4496dd0
wiks <wiks@wiks.eu>
2018-05-07 11:50:57 +0200

    działa, ustawienia z wyborem gośnika itd

---
+++ js/wiks_weather.js
@@ -0,0 +1,20 @@
+
+
+setTimeout(function(){ 
+    alert("Hello"); 
+    heartbeat_get_weather();
+}, 15000);
+
+/* wiks-heartbeat
+ *  
+ */
+function heartbeat_get_weather() {
+    
+    
+    
+    setTimeout(function(){ 
+        alert("Hello"); 
+        heartbeat_get_weather();
+    }, 15000);
+}
+
